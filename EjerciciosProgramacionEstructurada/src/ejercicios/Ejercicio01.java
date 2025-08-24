a601a45e7a1f573512f317bb7b41ee483a2a189f
JDGimenez73 <j.d.gimenez@hotmail.com.ar>
2025-08-24 17:08:38 -0300

    Ejercicio 01 - Año bisiesto

---
+++ EjerciciosProgramacionEstructurada/src/ejercicios/Ejercicio01.java
@@ -0,0 +1,21 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio01 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Ingrese un año: ");
+        int anio = scan.nextInt();
+
+        if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0)) {
+            System.out.println("El año " + anio + " es bisiesto.");
+        } else {
+            System.out.println("El año " + anio + " no es bisiesto.");
+        }
+
+        scan.close();
+    }
+}
