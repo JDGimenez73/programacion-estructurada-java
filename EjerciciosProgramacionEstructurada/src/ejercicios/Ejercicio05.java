c7a568b5ee97195be819e2775b5dfdddacd4e8b5
JDGimenez73 <j.d.gimenez@hotmail.com.ar>
2025-08-24 17:48:17 -0300

    Ejercicio 05

---
+++ EjerciciosProgramacionEstructurada/src/ejercicios/Ejercicio05.java
@@ -0,0 +1,21 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio05 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int suma = 0, num;
+
+        do {
+            System.out.print("Ingrese un número (0 para terminar): ");
+            num = scan.nextInt();
+            if (num % 2 == 0 && num != 0) {
+                suma += num;
+            }
+        } while (num != 0);
+
+        System.out.println("La suma de los números pares es: " + suma);
+        scan.close();
+    }
+}
