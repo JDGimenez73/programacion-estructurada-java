b747aa3da7f6e9fc4a049dba00c505b09478d13e
JDGimenez73 <j.d.gimenez@hotmail.com.ar>
2025-08-24 17:48:27 -0300

    Ejercicio 06

---
+++ EjerciciosProgramacionEstructurada/src/ejercicios/Ejercicio06.java
@@ -0,0 +1,26 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio06 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int positivos = 0, negativos = 0, ceros = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("Ingrese el número " + i + ": ");
+            int num = scan.nextInt();
+
+            if (num > 0) positivos++;
+            else if (num < 0) negativos++;
+            else ceros++;
+        }
+
+        System.out.println("Positivos: " + positivos);
+        System.out.println("Negativos: " + negativos);
+        System.out.println("Ceros: " + ceros);
+
+        scan.close();
+    }
+}
