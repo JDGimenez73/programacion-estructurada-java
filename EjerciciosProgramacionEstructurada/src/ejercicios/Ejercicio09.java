75cd5299594d5dec51abe6c2c1d6f59c26a3b365
JDGimenez73 <j.d.gimenez@hotmail.com.ar>
2025-08-24 17:48:58 -0300

    Ejercicio 09

---
+++ EjerciciosProgramacionEstructurada/src/ejercicios/Ejercicio09.java
@@ -0,0 +1,41 @@
+package ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio09 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Ingrese el precio del producto: ");
+        double precio = scan.nextDouble();
+
+        System.out.print("Ingrese el peso del paquete en kg: ");
+        double peso = scan.nextDouble();
+
+        System.out.print("Ingrese la zona de envío (Nacional/Internacional): ");
+        String zona = scan.next();
+
+        double costoEnvio = calcularCostoEnvio(peso, zona);
+        double total = calcularTotalCompra(precio, costoEnvio);
+
+        System.out.println("El costo de envío es: " + costoEnvio);
+        System.out.println("El total a pagar es: " + total);
+
+        scan.close();
+    }
+
+    public static double calcularCostoEnvio(double peso, String zona) {
+        if (zona.equalsIgnoreCase("Nacional")) {
+            return peso * 5;
+        } else if (zona.equalsIgnoreCase("Internacional")) {
+            return peso * 10;
+        } else {
+            System.out.println("Zona inválida.");
+            return 0;
+        }
+    }
+
+    public static double calcularTotalCompra(double precioProducto, double costoEnvio) {
+        return precioProducto + costoEnvio;
+    }
+}
